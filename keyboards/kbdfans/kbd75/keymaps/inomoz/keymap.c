7ad03b4119738398feccfef9636e348cafe4a03e
Inom T <i@inomoz.ru>
2023-05-19 01:16:30 +0600

    Adjust keyborad keys/layout

--- keyboards/kbdfans/kbd75/keymaps/inomoz/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/inomoz/keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 
-
 #define _LAYER0 0
 #define _LAYER1 1
 #define _LAYER2 2
@@ -33,46 +32,32 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Main layer
-    [_LAYER0] = LAYOUT(KC_ESC,           KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_SCRL, KC_PAUS,
-                       KC_GRV,           KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC, KC_DEL,
-                       KC_TAB,           KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_VOLU,
-                       KC_LCTL,          KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,                    KC_VOLD,
-                       KC_LSPO, KC_LSPO, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_RSPC,                   KC_MPLY,
-                       KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC,                    MO(1), KC_RGUI, KC_RCTL, KC_APP, TG(2),                    KC_MUTE),
+    //                    1       2           3           4              5           6            7         8             9           10          11          12          13               14       15       16
+    [_LAYER0] = LAYOUT(KC_ESC,  KC_F1,      KC_F2,      KC_F3,         KC_F4,      KC_F5,       KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,          KC_PSCR, KC_SCRL, KC_PAUS,
+                       KC_GRV,  KC_1,       KC_2,       KC_3,          KC_4,       KC_5,        KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,          KC_BSPC, KC_BSPC, KC_DEL,
+                       KC_TAB,  KC_Q,       KC_W,       KC_E,          KC_R,       KC_T,        KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,         KC_BSLS, KC_VOLU,
+                       KC_LCTL, KC_A,       KC_S,       KC_D,          KC_F,       KC_G,        KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    RCTL_T(KC_ENT),  KC_VOLD,
+                       SC_LSPO, SC_LSPO,    KC_Z,       KC_X,          KC_C,       KC_V,        KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    SC_RSPC,         SC_RSPC, KC_MPLY,
+                       KC_APP,  KC_LGUI,    KC_LALT,    KC_SPC,        KC_SPC,     KC_SPC,      MO(1),      KC_RCTL,    KC_LALT,    KC_NO,      KC_NO,      KC_NO),
 
-    // Function layer
-    [_LAYER1] = LAYOUT(RGB_TOG, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, RGB_M_T, KC_TRNS, KC_TRNS, KC_NO, RGB_SPI, RGB_SPD, KC_TRNS,
-                       KC_TRNS, LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5), LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), LGUI(KC_0), RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,
-                       KC_TRNS, KC_TRNS, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, KC_NO, KC_DOWN, KC_RGHT, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV,
-                       KC_TRNS, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MNXT,
-                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_NO, KC_NO, KC_UP, KC_LEFT, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MRWD,
-                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MFFD),
-
-    // Gaming layer
-    [_LAYER2] = LAYOUT(KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS,
-                       KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_BSPC, KC_NO,
-                       KC_TAB, KC_Q, KC_UP, KC_E, KC_M, KC_R, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_BSLS, KC_NO,
-                       KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_I, KC_G, KC_NO, KC_NO, KC_NO, KC_NO, KC_SCLN, KC_QUOT, KC_ENT, KC_NO,
-                       KC_LSFT, KC_LSFT, KC_Z, KC_X, KC_C, KC_K, KC_B, KC_NO, KC_NO, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_RSFT, KC_NO,
-                       KC_LCTL, KC_NO, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, KC_NO, KC_APP, KC_RCTL, KC_TRNS, KC_NO),
+    // Function layer 1
+    //                    1        2           3           4              5           6           7           8           9           10         11          12          13                14       15      16
+    [_LAYER1] = LAYOUT(RGB_TOG, KC_P1,      KC_P2,      KC_P3,         KC_P4,      KC_P5,       KC_P6,      KC_P7,      KC_P8,      KC_P9,      KC_P0,      KC_PAST,    KC_PPLS,         RGB_SPI, RGB_SPD, KC_TRNS,
+                       KC_TRNS, RGUI(KC_1), LGUI(KC_2), LGUI(KC_3),    LGUI(KC_4), LGUI(KC_5),  LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), LGUI(KC_0), KC_PMNS,    KC_PEQL,         KC_DEL,  RGB_VAD, KC_TRNS,
+                       KC_PGUP, KC_HOME,    KC_BTN1,    KC_UP,         KC_BTN2,    KC_TRNS,     LCTL(KC_C), KC_TRNS,    KC_BTN1,    KC_MS_U,    KC_BTN3,    KC_TRNS,    KC_TRNS,         KC_TRNS, KC_MPRV,
+                       KC_PGDN,  KC_END,     KC_LEFT,    KC_DOWN,       KC_RIGHT,   KC_TRNS,     KC_TRNS,    KC_TRNS,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_TRNS,    KC_TRNS,         KC_MNXT,
+                       KC_INS,  KC_TRNS,    KC_TRNS,    LCTL(KC_X),    KC_PGDN,    KC_TRNS,     KC_NO,      KC_TRNS,    KC_TRNS,    KC_PCMM,    KC_PDOT,    KC_PSLS,    KC_TRNS,         KC_TRNS, KC_MRWD,
+                       KC_NUM,  KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_MFFD),
 };
 
-// Seend Esc on pressing Ctrl+[ and releasing both keys
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case KC_LBRC:
-            if (record->event.pressed) {
-                if (get_mods() & MOD_BIT(KC_LCTL)) {
-                    // Remove modifers and send Esc
-                    del_mods(MOD_BIT(KC_LCTL));
-                    // send ESC onse
-                    register_code(KC_ESC);
-                    unregister_code(KC_ESC);
-                } else {
-                    register_code(KC_LBRC);
-                }
-            }
-            return false;
-    }
-    return true;
-}
+// Initialize variable holding the binary
+// representation of active modifiers.
+// https://docs.qmk.fm/#/feature_advanced_keycodes?id=key-overrides
+// https://github.com/qmk/qmk_firmware/blob/master/docs/feature_key_overrides.md
+const key_override_t vim_escape_map = ko_make_basic(MOD_MASK_CTRL, KC_LBRC, KC_ESC);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &vim_escape_map,
+    NULL // Null terminate the array of overrides!
+};
--- keyboards/kbdfans/kbd75/rules.mk
+++ keyboards/kbdfans/kbd75/rules.mk
@@ -1,1 +1,2 @@
 DEFAULT_FOLDER = kbdfans/kbd75/rev1
+KEY_OVERRIDE_ENABLE = yes
