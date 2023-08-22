37a719192282324a34d64253aaefdf62a335490c
Inom T <i@inomoz.ru>
2023-08-22 12:48:07 +0600

    Update my layout

--- keyboards/kbdfans/kbd75/keymaps/inomoz/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/inomoz/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_TAB,  KC_Q,       KC_W,       KC_E,          KC_R,       KC_T,        KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,         KC_BSLS, KC_VOLU,
                        KC_LCTL, KC_A,       KC_S,       KC_D,          KC_F,       KC_G,        KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    RCTL_T(KC_ENT),  KC_VOLD,
                        SC_LSPO, SC_LSPO,    KC_Z,       KC_X,          KC_C,       KC_V,        KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    SC_RSPC,         SC_RSPC, KC_MPLY,
-                       KC_APP,  KC_LGUI,    KC_LALT,    KC_SPC,        KC_SPC,     KC_SPC,      MO(2),      KC_RCTL,    KC_LALT,    TG(1),      KC_PGUP,      KC_PGDN),
+                       KC_APP,  KC_LGUI,    KC_LALT,    KC_SPC,        KC_SPC,     KC_SPC,      MO(2),      KC_RCTL,    KC_LALT,    KC_F13,      KC_PGUP,      KC_PGDN),
 
     //                    1       2           3           4              5           6            7         8             9           10          11          12          13               14       15       16
     [_LAYER1] = LAYOUT(KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS,
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_TRNS, KC_TRNS,    KC_TRNS,    KC_F,          KC_P,       KC_B,        KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    KC_TRNS,    KC_TRNS,         KC_TRNS, KC_TRNS,
                        KC_TRNS, KC_TRNS,    KC_R,       KC_S,          KC_T,       KC_G,        KC_M,       KC_N,       KC_E,       KC_I,       KC_O,       KC_TRNS,    KC_TRNS,         KC_TRNS,
                        KC_TRNS, KC_TRNS,    KC_X,       KC_C,          KC_D,       KC_V,        KC_Z,       KC_K,       KC_H,       KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,         KC_TRNS, KC_TRNS,
-                       KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS),
+                       KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    TG(0)),
 
     // Function layer 1
     //                    1        2           3           4              5           6           7           8           9           10         11          12          13                14       15      16
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_PGUP, KC_HOME,    KC_BTN1,    KC_UP,         KC_BTN2,    KC_TRNS,     LCTL(KC_C), KC_WH_U,    KC_BTN1,    KC_MS_U,    KC_BTN3,    KC_TRNS,    KC_TRNS,         KC_TRNS, KC_MPRV,
                        KC_PGDN, KC_END,     KC_LEFT,    KC_DOWN,       KC_RIGHT,   KC_TRNS,     KC_TRNS,    KC_WH_D,    KC_MS_L,    KC_MS_D,    KC_MS_R,    KC_TRNS,    KC_TRNS,         KC_MNXT,
                        KC_INS,  KC_TRNS,    KC_TRNS,    LCTL(KC_X),    KC_PGDN,    KC_TRNS,     KC_NO,      KC_TRNS,    KC_TRNS,    KC_PCMM,    KC_PDOT,    KC_PSLS,    KC_TRNS,         KC_TRNS, KC_MRWD,
-                       KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,       KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS),
+                       KC_TRNS, KC_TRNS,    KC_TRNS,    KC_F14,        KC_F14,     KC_F14,     KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    TG(1)),
 };
 
 // Initialize variable holding the binary
